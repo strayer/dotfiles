b36ad2d7806a9cef86559a31e2fc12650c1253ef
Sven Grunewaldt <strayer@olle-orks.org>
2018-02-23 13:22:47 +0100

    fish: set env vars for linuxbrew on sector001

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -32,6 +32,13 @@ if test $host = "wolf359" -a $user = "strayer"
   end
 end
 
+if test $host = "sector001" -a $user = "strayer"
+  # Linuxbrew
+  set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+  set -gx MANPATH /home/linuxbrew/.linuxbrew/share/man (manpath -g)
+  set -gx INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
+end
+
 # Local software & scripts
 if test -d "$HOME/.bin"
   set -gx PATH "$HOME/.bin" $PATH
