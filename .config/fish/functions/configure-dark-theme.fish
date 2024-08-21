7ac92130f56c6a0b1c5e9e05ffef80560daa2245
Sven Grunewaldt <strayer@olle-orks.org>
2024-08-21 22:01:22 +0200

    chore(fish): remove old theme configuration functions

--- .config/fish/functions/configure-dark-theme.fish
+++
@@ -1,9 +0,0 @@
-function configure-dark-theme
-  yes | fish_config theme save 'tokyonight_storm'
-  command -v kitty && kitty +kitten themes --reload-in=all "Tokyo Night Storm"
-
-  set -Ux BAT_THEME OneHalfDark
-  command -v vivid && set -Ux LS_COLORS (vivid generate snazzy)
-  set -Ux TERMINAL_THEME dark
-end
-
