0ee2faf7d5099d9f426a65c0f87c5165d0fc980e
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2026-04-08 17:26:35 +0200

    feat(fish): enable 1password in safehouse when op cli is available

--- dot_config/fish/functions/safe.fish
+++ dot_config/fish/functions/safe.fish
@@ -1,3 +1,7 @@
 function safe
-    safehouse --add-dirs-ro="$HOME/.global_gitignore" --add-dirs-ro="$HOME/.bin/secretive-ssh-keygen" $argv
+    set -l extra_args
+    if command -q op
+        set extra_args --enable=1password
+    end
+    safehouse --add-dirs-ro="$HOME/.global_gitignore" --add-dirs-ro="$HOME/.bin/secretive-ssh-keygen" $extra_args $argv
 end
