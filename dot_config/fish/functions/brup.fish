e6652d8871df1773e043cfbb5d8bb569c21bb75a
Sven Grunewaldt <s.grunewaldt@westfalen.com>
2026-04-08 14:06:17 +0200

    feat(fish): add --caffeinate flag to brup function

--- dot_config/fish/functions/brup.fish
+++ dot_config/fish/functions/brup.fish
@@ -1,4 +1,13 @@
 function brup
+    argparse caffeinate -- $argv
+    or return
+
+    if set -q _flag_caffeinate
+        echo "### Wrapping brup with caffeinate -dims…"
+        caffeinate -dims fish -c brup
+        return
+    end
+
     echo "### Running brew up…"
     brew up; or return
     brew upgrade; or return
